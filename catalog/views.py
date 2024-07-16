4b2d9cc7cf9cb417de4a8b2bfd513877e56295c5
Alina Baranova <alina_nemo@mail.ru>
2024-07-16 17:31:58 +0300

    Create json file for recording

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,18 @@
+import json
+
 from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        data = {'Name': name, 'Number': phone, 'Message': message}
+        with open('data.json', 'w', encoding='UTF-8') as file:
+            json.dump(data, file)
     return render(request, 'contacts.html')
